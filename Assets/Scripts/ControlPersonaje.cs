6a8444e5f78c06e8bd8c2484abc9efd8fc0ad31e
Kobkev <kevin.verelstg@gmail.com>
2021-10-19 17:07:04 -0500

    Avance Proyecto 3 (Kevin)
    -Agregué un desvanecimiendo como cinemática al iniciar el juego.
    -Agregué particula para items (Objetos)
    -Reparé un bug con el modo de caminar
    -Agregué botón de correr, animación de correr y script apropiado para hacerlo.
    -Modifique los filtros y effectos de la camara para verse más cinemático y parecido a ser un sueño.
    -Agregué sonido de ambientación con sonido espacial 3d
    -Modifiqué ligeramente la iluminación del personaje.
    -Agregué assets básicos para crear más effectos de particulas.
    -Particulas aparecen de distintos colores y tienen effectos de desvanecimiento. además de contener brillo
    -Modifiqué ligeramente los triggers de la habitación.

--- Assets/Scripts/ControlPersonaje.cs
+++ Assets/Scripts/ControlPersonaje.cs
@@ -1,36 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ControlPersonaje : MonoBehaviour{
 
     public GameObject elPersonaje;
     public bool enMovimiento;
+    bool estaCorriendo;
     float movimientoHorizontal;
     float movimientoVertical;
     [SerializeField] float velocidadRotacion = 150f; 
-    [SerializeField] float velocidadCaminar= 1.5f;
+    [SerializeField] float velocidadCaminar= 0.5f;
+    float velocidadCorrer= .01f;
 
     void Update(){
+        if(Input.GetKey(KeyCode.LeftShift)){
+            estaCorriendo = true;
+        } else {
+            estaCorriendo = false;
+        }
         if(Input.GetButton("Horizontal") || Input.GetButton("Vertical")){
             if(Input.GetKey(KeyCode.S)){
                 movimientoVertical = Input.GetAxis("Vertical") * Time.deltaTime * velocidadCaminar/2;
-                elPersonaje.transform.Translate(0, 0, movimientoVertical);
                 elPersonaje.GetComponent<Animator>().Play("WalkBack");
             } else {
-                movimientoVertical = Input.GetAxis("Vertical") * Time.deltaTime * velocidadCaminar;
-                elPersonaje.transform.Translate(0, 0, movimientoVertical);
-                //elPersonaje.GetComponent<Animator>().Play("StartWalk");
-                elPersonaje.GetComponent<Animator>().Play("Walk");
-            }
+                if (estaCorriendo == false){
+                    movimientoVertical = Input.GetAxis("Vertical") * Time.deltaTime * velocidadCaminar;
+                    //elPersonaje.GetComponent<Animator>().Play("StartWalk");
+                    elPersonaje.GetComponent<Animator>().Play("Walk");
+                } else if(Input.GetKey(KeyCode.W) && estaCorriendo == true) {
+                    movimientoVertical = Input.GetAxis("Vertical") * Time.deltaTime * velocidadCaminar * velocidadCorrer;
+                    elPersonaje.GetComponent<Animator>().Play("Run");
+                }
+            } 
             movimientoHorizontal = Input.GetAxis("Horizontal") * Time.deltaTime * velocidadRotacion;
-            
             elPersonaje.transform.Rotate(0, movimientoHorizontal, 0);
-            
+            elPersonaje.transform.Translate(0, 0, movimientoVertical);
         } else {
             enMovimiento = false;
             elPersonaje.GetComponent<Animator>().Play("Idle");
-        }   
-
+        }  
     }
 }
